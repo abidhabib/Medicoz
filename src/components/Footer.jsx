d0aedd99ab867fe3734282055dfabb7cbfb4284c
Abid Habib <abid.habib.2018@gmail.com>
2023-06-12 02:51:09 +0500

    footer-adjust

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -26,35 +26,35 @@ const Footer = () => {
                         <ul className="box">
                             <li className="link_name">Our location
                             </li>
-                            <li><a href="#">Bronx Park Urgent Medical Care
-                            </a></li>
-                            <li><a href="#">2016 Bronxdale Ave
-                            </a></li>
-                            <li><a href="#">Suite #101</a></li>
-                            <li><a href="#">Bronx, NY 10462
+                            <li>Bronx Park Urgent Medical Care
+                            </li>
+                            <li>2016 Bronxdale Ave
+                            </li>
+                            <li>Suite #101</li>
+                            <li>Bronx, NY 10462
 
-                            </a></li>
+                            </li>
                         </ul>
                         <ul className="box">
                             <li className="link_name">Get in touch
                             </li>
-                            <li><a href="#">(718) 918-9676
+                            <li>(718) 918-9676
 
-                            </a></li>
-                            <li><a href="#">bronxparkurgentmedicalcare@gmail.com
+                            </li>
+                            <li>bronxparkurgentmedicalcare@gmail.com
 
 
-                            </a></li>
+                            </li>
 
                         </ul>
                         <ul className="box">
                             <li className="link_name">Hours of Operation
                             </li>
-                            <li><a href="#">Monday – Friday 9:00 am to 7:00 pm
-                            </a></li>
-                            <li><a href="#">JSaturday 9:00 am to 5:00 pm
-                            </a></li>
-                            <li><a href="#">Closed on Sunday</a></li>
+                            <li>Monday – Friday 9:00 am to 7:00 pm
+                            </li>
+                            <li>JSaturday 9:00 am to 5:00 pm
+                           </li>
+                            <li>Closed on Sunday</li>
                         </ul>
 
                     </div>
@@ -64,7 +64,7 @@ const Footer = () => {
                         <span className="copyright_text">©2023 Bronx Park Urgent Medical Care. All rights reserved.
                         </span>
                         <span className="policy_terms">
-                            <a href="#">Insurance</a>
+                           Insurance
                             <a href="#">Our Team</a>
                             <a href="#">Services</a>
 
